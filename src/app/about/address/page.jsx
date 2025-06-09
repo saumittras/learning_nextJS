6248bcb5a73519f4458060478a7a6c1d1bb0a178
saumittras <saumittra.eee@gmail.com>
2025-06-09 22:27:09 +0600

    learn sub routing concept

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Address() {
+  return (
+    <div>
+      <h4>Address Pages as sub route pages</h4>
+    </div>
+  )
+}
---
+++ src/app/services/page.jsx
@@ -0,0 +1,8 @@
+
+export default function ServicesPage() {
+  return (
+    <div>
+        <h3 className='text-5xl'>Services page</h3>
+    </div>
+  )
+}
